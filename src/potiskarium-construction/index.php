12cc9c11ddddfee45b2710ecabce9d7c368ea4f4
Karel Zavadil <zavadil@incomaker.com>
2025-11-21 00:35:04 +0100

    construction minimal theme

---
+++ src/potiskarium-construction/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title><?php bloginfo('name'); ?></title>
+	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css">
+</head>
+<body>
+	<img src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" alt="Logo">
+</body>
+</html>
