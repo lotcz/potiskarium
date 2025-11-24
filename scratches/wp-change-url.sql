2bb8d8df7b2fe34ea2381e9d5893492936c764ca
Karel Zavadil <zavadil@incomaker.com>
2025-11-25 00:13:20 +0100

    mvp

--- scratches/wp-change-url.sql
+++ scratches/wp-change-url.sql
@@ -1,5 +1,5 @@
 UPDATE wp_options SET option_value = replace(option_value, 'potiskarium.loc', 'potiskarium.cz')
  WHERE option_name = 'home' OR option_name = 'siteurl';
- UPDATE wp_posts SET guid = replace(guid, 'oldurl.com','newurl.com');
- UPDATE wp_posts SET post_content = replace(post_content, 'oldurl.com', 'newurl.com');
- UPDATE wp_postmeta SET meta_value = replace(meta_value,'oldurl.com','newurl.com');
+ UPDATE wp_posts SET guid = replace(guid, 'potiskarium.loc', 'potiskarium.cz');
+ UPDATE wp_posts SET post_content = replace(post_content, 'potiskarium.loc', 'potiskarium.cz');
+ UPDATE wp_postmeta SET meta_value = replace(meta_value,'potiskarium.loc', 'potiskarium.cz');
