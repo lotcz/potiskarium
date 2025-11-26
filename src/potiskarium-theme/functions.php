b2ade79a899b552d55e9b41c25704579f4ba6a94
Karel Zavadil <zavadil@incomaker.com>
2025-11-26 04:17:42 +0100

    static block with build and localized script

--- src/potiskarium-theme/functions.php
+++ src/potiskarium-theme/functions.php
@@ -24,7 +24,36 @@ function potiskarium_theme_scripts() {
 add_action('wp_enqueue_scripts', 'potiskarium_theme_scripts');
 
 /* custom blocks */
+
+/**
+ * Registers a custom block category for your theme's blocks.
+ *
+ * @param array $categories Array of block categories.
+ * @return array Filtered array of block categories.
+ */
+function my_custom_block_categories($categories) {
+	$new_category = array(
+		'slug'  => 'potiskarium-theme-blocks',
+		'title' => 'Potiskarium',
+		'icon'  => 'star-filled',
+	);
+
+	return array_merge(array($new_category), $categories);
+}
+
+add_filter( 'block_categories_all', 'my_custom_block_categories' );
+
 add_action('init', function () {
 	register_block_type(__DIR__ . '/build/logo-block');
+});
+
+add_action('enqueue_block_editor_assets', function() {
+	wp_localize_script(
+		'potiskarium-theme-logo-block-editor-script',  // Format: {namespace}-{block-name}-editor-script
+		'PotiskariumThemeData',
+		array(
+			'themeUri' => get_stylesheet_directory_uri()
+		)
+	);
 });
 
--- src/potiskarium-theme/src/logo-block/editor.js
+++ src/potiskarium-theme/src/logo-block/editor.js
@@ -1,7 +1,16 @@
 import { registerBlockType } from '@wordpress/blocks';
-import { useBlockProps } from '@wordpress/block-editor';
+import { useBlockProps, InspectorControls } from '@wordpress/block-editor';
 import { SelectControl } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
+
+function potiskariumLogoUrl(variant) {
+	const THEME_BASE_URI = (typeof PotiskariumThemeData !== 'undefined') ? PotiskariumThemeData.themeUri : '';
+
+	if (!variant) {
+		variant = 'logo-white';
+	}
+	return THEME_BASE_URI + '/img/' + variant + '.svg';
+}
 
 registerBlockType( 'potiskarium-theme/logo-block', {
 	edit: ( { attributes, setAttributes } ) => {
@@ -13,24 +22,33 @@ registerBlockType( 'potiskarium-theme/logo-block', {
 		};
 
 		return (
-			<div { ...blockProps }>
-				{/* 2. Variant Control (in Inspector Controls, which appear in the sidebar) */}
-				<div style={{ marginTop: '1em' }}>
-					<SelectControl
-						label={ __( 'Image Variant', 'simple-image-block' ) }
-						value={ variant }
-						options={ [
-							{ label: 'White', value: "logo-white" },
-							{ label: 'Purple', value: "logo" },
-						] }
-						onChange={ onVariantChange }
-					/>
+			<>
+				<InspectorControls>
+					<div style={{ marginTop: '1em' }}>
+						<SelectControl
+							label={ __( 'Image Variant', 'simple-image-block' ) }
+							value={ variant }
+							options={ [
+								{ label: 'White', value: "logo-white" },
+								{ label: 'Purple', value: "logo" },
+							] }
+							onChange={ onVariantChange }
+						/>
+					</div>
+				</InspectorControls>
+
+				<div { ...blockProps }>
+					<img src={potiskariumLogoUrl(variant)} alt="potiskarium-logo" />
 				</div>
-			</div>
+			</>
 		);
 	},
-	save: () => {
-		// Dynamic blocks should return null for the save function
-		return null;
+	save: ({ attributes }) => {
+		const blockProps = useBlockProps.save();
+		const { variant } = attributes;
+
+		return <div { ...blockProps }>
+			<img src={potiskariumLogoUrl(variant)} alt="Potiskarium" />
+		</div>;
 	},
 } );
