f525cf6b4988da397fb7d29415d35319afc95049
Karel Zavadil <zavadil@incomaker.com>
2025-11-26 05:14:26 +0100

    added number input

--- src/potiskarium-theme/src/logo-block/editor.js
+++ src/potiskarium-theme/src/logo-block/editor.js
@@ -1,32 +1,44 @@
 import { registerBlockType } from '@wordpress/blocks';
 import { useBlockProps, InspectorControls } from '@wordpress/block-editor';
-import { SelectControl } from '@wordpress/components';
+import { SelectControl, __experimentalUnitControl as UnitControl } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 
 function potiskariumLogoUrl(variant) {
 	const THEME_BASE_URI = (typeof PotiskariumThemeData !== 'undefined') ? PotiskariumThemeData.themeUri : '';
-
 	if (!variant) {
 		variant = 'logo-white';
 	}
+
 	return THEME_BASE_URI + '/img/' + variant + '.svg';
+}
+
+function potiskariumLogoStyle(maxHeight) {
+	if (!maxHeight) {
+		maxHeight = 200;
+	}
+
+	return {maxHeight: maxHeight};
 }
 
 registerBlockType( 'potiskarium-theme/logo-block', {
 	edit: ( { attributes, setAttributes } ) => {
 		const blockProps = useBlockProps();
-		const { variant } = attributes;
+		const { variant, maxHeight } = attributes;
 
 		const onVariantChange = ( newVariant ) => {
 			setAttributes( { variant: newVariant } );
 		};
 
+		const onMaxHeightChange = ( h ) => {
+			setAttributes( { maxHeight: h } );
+		};
+
 		return (
 			<>
 				<InspectorControls>
-					<div style={{ marginTop: '1em' }}>
+					<div style={{ margin: '1em' }}>
 						<SelectControl
-							label={ __( 'Image Variant', 'simple-image-block' ) }
+							label={ __( 'Image Variant') }
 							value={ variant }
 							options={ [
 								{ label: 'White', value: "logo-white" },
@@ -34,21 +46,38 @@ registerBlockType( 'potiskarium-theme/logo-block', {
 							] }
 							onChange={ onVariantChange }
 						/>
+						<UnitControl
+							label={ __( 'Max. height') }
+							value={ maxHeight }
+							required={false}
+							onChange={ onMaxHeightChange }
+							units={[
+								{ value: 'px', label: 'px' }
+							]}
+						/>
 					</div>
 				</InspectorControls>
 
 				<div { ...blockProps }>
-					<img src={potiskariumLogoUrl(variant)} alt="potiskarium-logo" />
+					<img
+						src={potiskariumLogoUrl(variant)}
+						alt="potiskarium-logo"
+						style={potiskariumLogoStyle(maxHeight)}
+					/>
 				</div>
 			</>
 		);
 	},
 	save: ({ attributes }) => {
 		const blockProps = useBlockProps.save();
-		const { variant } = attributes;
+		const { variant, maxHeight } = attributes;
 
 		return <div { ...blockProps }>
-			<img src={potiskariumLogoUrl(variant)} alt="Potiskarium" />
+			<img
+				src={potiskariumLogoUrl(variant)}
+				alt="Potiskarium"
+				style={potiskariumLogoStyle(maxHeight)}
+			/>
 		</div>;
 	},
 } );
